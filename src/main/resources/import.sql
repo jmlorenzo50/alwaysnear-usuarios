fe554d8c972bf1b6d0527839fb16528f6070706a
jorge <c0de42.com@gmail.com>
2019-08-14 10:06:31 +0200

    Clave securizadas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO `usuario` (usu_login, usu_password, usu_activo) VALUES ('admin','123456',1);
-INSERT INTO `usuario` (usu_login, usu_password, usu_activo) VALUES ('jorge','123456',1);
+INSERT INTO `usuario` (usu_login, usu_password, usu_activo) VALUES ('admin','$2a$10$6wd0XgG/Hue4IzqTpW3R9OHIPwlu65udVhp51ylAumPOCFPau7eLy',1);
+INSERT INTO `usuario` (usu_login, usu_password, usu_activo) VALUES ('jorge','$2a$10$6wd0XgG/Hue4IzqTpW3R9OHIPwlu65udVhp51ylAumPOCFPau7eLy',1);
 
 INSERT INTO `rol` (rol_nombre) VALUES ('ROLE_USER');
 INSERT INTO `rol` (rol_nombre) VALUES ('ROLE_ADMIN');
